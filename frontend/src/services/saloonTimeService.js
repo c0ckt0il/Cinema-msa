15af5f5e3e40ed20d6bc63499ed12f5e72b3f3c1
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2024-08-08 13:11:50 +0900

    Update saloonTimeService.js

--- frontend/src/services/saloonTimeService.js
+++ frontend/src/services/saloonTimeService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class SaloonTimeService {
 
-    apiUrl = "http://localhost:8080/api/movie/movieSaloonTimes/"
+    apiUrl = "http://movie:8080/api/movie/movieSaloonTimes/"
 
     getMovieSaloonTimeSaloonAndMovieId(saloonId, movieId) {
         return axios.get(this.apiUrl + "getMovieSaloonTimeSaloonAndMovieId/" + saloonId + "/" + movieId);
