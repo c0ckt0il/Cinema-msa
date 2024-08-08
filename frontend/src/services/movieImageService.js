68713390e05853e59896d2fca1f3e14c86bd3f40
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2024-08-08 13:11:26 +0900

    Update movieImageService.js

--- frontend/src/services/movieImageService.js
+++ frontend/src/services/movieImageService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class MovieImageService {
 
-    apiUrl = "http://localhost:8080/api/movie/images/"
+    apiUrl = "http://movie:8080/api/movie/images/"
     
     addMovieImage(imageDto) {
         return axios.post(this.apiUrl + "add", imageDto);
